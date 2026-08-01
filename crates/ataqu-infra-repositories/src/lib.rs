b65638e2d3a35a31f1fa81cefe720dc720cb6401
elcoosp <elcoosp@gmail.com>
2026-08-01 17:07:28 +0200

    fix(cinq): use raw SQL for GIN indexes in migration

--- crates/ataqu-infra-repositories/src/lib.rs
+++ crates/ataqu-infra-repositories/src/lib.rs
@@ -3,3 +3,15 @@ pub mod batch_helper;
 pub mod cinq_repo;
 
 pub mod generic_batch;
+
+pub mod entities;
+
+pub mod email_tracking_writer;
+
+pub mod transactional_batch_insert;
+
+pub mod cinq_contact_repo;
+
+pub mod cinq_deal_repo;
+
+pub mod cinq_csv_import;
