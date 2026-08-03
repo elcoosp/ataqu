5b154102a41840df80d1d0b2a9ede70f9d126b12
elcoosp <elcoosp@gmail.com>
2026-08-03 11:15:40 +0200

    fix(routes): rename routes/index.ts to router.ts to avoid conflict with index.tsx

--- apps/vista/src/App.tsx
+++ apps/vista/src/App.tsx
@@ -1,6 +1,6 @@
 import { Shell } from '@ataqu/ui';
 import { RouterProvider } from '@tanstack/react-router';
-import { router } from './routes';
+import { router } from './routes/router';
 
 export const App = () => (
   <Shell activeApp="vista">
