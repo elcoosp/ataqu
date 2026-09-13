0f1b30373176fd11c29087dd8f93791eafb863ab
elcoosp <elcoosp@gmail.com>
2026-09-13 18:35:17 +0200

    migrate(ataqu): apps/dial/src/components/message-input.tsx -> apps/ataqu/src/apps/dial/components/message-input.tsx

--- apps/ataqu/src/apps/dial/components/message-input.tsx
+++ apps/ataqu/src/apps/dial/components/message-input.tsx
@@ -1,6 +1,8 @@
 import { useSendMessage } from "@ataqu/api-client";
 import { useIdempotency } from "@ataqu/shared-hooks";
-import { Button } from "@ataqu/ui";
+import {
+ Button 
+} from "@ataqu/ui";
 import { t } from "@lingui/core/macro";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Loader2, Send } from "lucide-react";
