17205cbaee2b3c46a54bd335efe06d1f2af7664f
elcoosp <elcoosp@gmail.com>
2026-09-13 18:35:24 +0200

    migrate(ataqu): apps/vault/src/components/cross-app-badge.tsx -> apps/ataqu/src/apps/vault/components/cross-app-badge.tsx

--- apps/ataqu/src/apps/vault/components/cross-app-badge.tsx
+++ apps/ataqu/src/apps/vault/components/cross-app-badge.tsx
@@ -1,6 +1,8 @@
 import { api } from "@ataqu/api-client";
 import type { UUID } from "@ataqu/types";
-import { Badge } from "@ataqu/ui";
+import {
+ Badge 
+} from "@ataqu/ui";
 import { t } from "@lingui/core/macro";
 import { Trans } from "@lingui/react/macro";
 import { useQuery } from "@tanstack/react-query";
