5bc654d44d59645a4fd8c419eeb63ee8f5f1f8c6
elcoosp <elcoosp@gmail.com>
2026-09-13 18:35:31 +0200

    migrate(ataqu): remove apps/dial/src/routes/_auth.tickets/$id.tsx

--- apps/dial/src/routes/_auth.tickets/$id.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { TicketDetail } from "@/components/ticket-detail";
-
-export const Route = createFileRoute("/_auth/tickets/$id")({
-	component: TicketDetail,
-});
