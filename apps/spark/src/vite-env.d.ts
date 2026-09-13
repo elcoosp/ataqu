d871a82fb7c3500843e4fcd0745b22f7c39cec15
elcoosp <elcoosp@gmail.com>
2026-09-13 18:35:40 +0200

    migrate(ataqu): remove apps/spark/src/vite-env.d.ts

--- apps/spark/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-declare module "*.css";
-declare module "@xyflow/react/dist/style.css";
-interface ImportMetaEnv {
-	readonly VITE_API_BASE_URL?: string;
-}
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
