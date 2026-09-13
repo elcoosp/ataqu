73c27429050d275df97c4da0bbcdd7aaa3f54922
elcoosp <elcoosp@gmail.com>
2026-09-13 18:35:15 +0200

    migrate(ataqu): apps/aegis/src/routes/register.tsx -> apps/ataqu/src/routes/register.tsx

--- apps/ataqu/src/routes/register.tsx
+++ apps/ataqu/src/routes/register.tsx
@@ -1,4 +1,6 @@
-import { RegisterForm } from "@ataqu/ui";
+import {
+ RegisterForm 
+} from "@ataqu/ui";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/register")({
