29607e7ee14e1925c60dda653bc9c7cec1a66452
elcoosp <elcoosp@gmail.com>
2026-09-20 19:01:38 +0200

    chore(ataqu): merge vitest config imports

--- apps/ataqu/vitest.config.ts
+++ apps/ataqu/vitest.config.ts
@@ -1,5 +1,4 @@
-import { mergeConfig } from "vitest/config";
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 import baseConfig from "./vite.config";
 
 /**
