43c1f79e1b6cbb43ac2d346fad83fcfa4a08128b
elcoosp <elcoosp@gmail.com>
2026-09-22 19:43:13 +0200

    feat(stores): export intent module

--- packages/shared-stores/src/index.ts
+++ packages/shared-stores/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./activation";
 export * from "./auth";
 export * from "./changelog";
+export * from "./intent";
 export * from "./onboarding";
 export * from "./selection";
 export * from "./ui";
